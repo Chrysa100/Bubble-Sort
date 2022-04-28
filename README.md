b2ef402555632004e517d995a1bff8744d8be97e
Chrysa <chrysapa@gmail.com>
2022-04-28 21:35:55 +0300

    finish exercise

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Bubble---sort
+# Bubble sort
+
+A small exercise from The Odin Project
+https://www.theodinproject.com/lessons/ruby-bubble-sort
---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array)
+  p array
+  flag = 1 
+  temp = 0 
+  while flag != 0 do
+    flag = 0
+    array.each_with_index do |number,index|  
+      if index < array.length-1
+        if number > array[index+1]
+          #p index
+          temp = number
+          array[index] = array[index+1]
+          array[index+1] = temp
+          flag = 1
+        end 
+      end
+       p array
+    end    
+  end
+end
+  bubble_sort([4,3,78,2,0,2])
+# => [0,2,2,3,4,78]
